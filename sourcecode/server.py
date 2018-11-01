10d7e67d1ccd2f50b2f30523c1c6ebd081baaec4
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-11-01 20:03:40 +0000

    Implementing user signup post route

--- sourcecode/server.py
+++ sourcecode/server.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, url_for, jsonify, request, redirect
+from flask import Flask, render_template, url_for, jsonify, request, redirect, session
 from flask_bootstrap import Bootstrap
-import ConfigParser, logging, os, json, random, re, string, time
+from flask_pymongo import PyMongo
+import ConfigParser, logging, os, json, random, re, string, datetime, bcrypt
 from logging.handlers import RotatingFileHandler
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-
 @app.route('/')
 def index():
+	if 'email' in session:
+		return render_template('index.html', email=session['email']) 
 	return render_template('index.html')
 
 
@@ -20,6 +22,46 @@ def error_400(e):
 def error_500(e):
 	return render_template('error.html', error=500), 500
 
+@app.route('/login')
+def login():
+	return ""
+
+@app.route('/signup', methods=['POST'])
+def signup():
+	if request.method == 'POST':
+		current_datetime = datetime.datetime.now()
+
+		first_name = request.form['first-name']
+		last_name = request.form['last-name']
+		email = request.form['email']
+		created = current_datetime
+		last_updated = current_datetime
+		is_admin = False
+		followers = []
+		following = []
+
+		users = mongo.db.users
+
+		existing_user = users.find_one({'email' : request.form['email']})
+
+		if existing_user is None:
+			hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+			users.insert({
+				'first_name' : first_name,
+				'last_name' : last_name,
+				'email' : email,
+				'password' : hashpass,
+				'created' : created,
+				'last_updated' : last_updated,
+				'is_admin' : is_admin,
+				'followers' : followers,
+				'following' : following
+				})
+			session['email'] = request.form['email']
+			return redirect('/')
+		
+		return "That email address already exists"
+	return ""
 
 def init(app):
 	config = ConfigParser.ConfigParser()
@@ -35,6 +77,9 @@ def init(app):
 		app.config['log_location'] = config.get("logging", "LOCATION")
 		app.config['log_file'] = config.get("logging", "NAME")
 		app.config['log_level'] = config.get("logging", "LEVEL")
+		
+		app.config['MONGO_DBNAME'] = "compound-lifts"
+		app.config["MONGO_URI"] = "mongodb://server:connector00@ds111192.mlab.com:11192/compound-lifts"
 	except:
 		print ("Could not read configs from: ", config_location)
 
@@ -54,9 +99,13 @@ def logs(app):
 if __name__ == '__main__':
 	init(app)
 	logs(app)
+	mongo = PyMongo(app)
+	app.secret_key = 'lift-compound-'
 	app.run(
 		host=app.config['ip_address'],
 		port=int(app.config['port']))
 else:
 	init(app)
 	logs(app)
+	mongo = PyMongo(app)
+	app.secret_key = 'lift-compound-'
