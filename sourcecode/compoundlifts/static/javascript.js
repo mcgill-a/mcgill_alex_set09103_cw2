4c77bfd66c495742e177e3fcd921629a996c2bca
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-11-23 22:09:02 +0000

    Implemented athlete lift section smooth scroll

--- sourcecode/compoundlifts/static/javascript.js
+++ sourcecode/compoundlifts/static/javascript.js
@@ -1,37 +1,3 @@
-// Get modal element
-var modal = document.getElementById("userModal");
-// Get modal buttons
-var modalSignIn = document.getElementById("modalSignIn");
-var modalSignUp = document.getElementById("modalSignUp");
-var modalCloseBtn = document.getElementsByClassName("closeBtn")[0];
-
-//window.addEventListener('click', clickOutside);
-
-function openModal(tab)
-{
-  openTab(tab);
-  modal.style.display = "block";
-}
-
-/*
-modalCloseBtn.addEventListener('click', closeModal);
-
-function closeModal()
-{
-  modal.style.display = "none";
-}
-*/
-
-// Close Modal if outside click
-/*
-function clickOutside(e)
-{
-  if(e.target == modal)
-  {
-    modal.style.display = "none";
-  }
-}
-*/
 
 // Open the selected modal tab
 function openTab(tab)
@@ -78,13 +44,4 @@ function openTab(tab)
     document.getElementById("tab-program").classList.remove('active');
     document.getElementById("tab-followers").classList.remove('active');
   }
-}
-
-
-function scrollTo(element) {
-  window.scroll({
-    top: element.getBoundingClientRect().top + window.scrollY,
-    left: 0,
-    behavior: 'smooth'
-  });
 }
