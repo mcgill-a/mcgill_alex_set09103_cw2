8b578df7a47f21afbd722ce0b053dd4bbaeee6c6
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-11-25 20:52:10 +0000

    Added example config file and updated ignore file

---
+++ sourcecode/compoundlifts/config/defaults_example.py
@@ -0,0 +1,14 @@
+# Mail client configuration
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+# Mail login info
+MAIL_USERNAME = "YOUR_EMAIL_ADDRESS_HERE"
+MAIL_PASSWORD = "YOUR_PASSWORD_HERE"
+# Datebase connection info
+MONGO_DBNAME = "compound-lifts"
+MONGO_URI = "mongodb://<<YOUR_DB_CONNECTION_URI>>"
+# Secret Key
+SECRET_KEY = "YOUR_SECRET_KEY"
+# ipinfo.io access token
+IP_INFO_TOKEN = "YOUR_IP_INFO_TOKEN"
